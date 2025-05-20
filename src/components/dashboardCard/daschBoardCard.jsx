75aa087c96305980bcf8baaa672966acd3cf8cc2
vicenteaugusto9 <vicenteaugusto180374@gmail.com>
2025-05-19 22:28:45 -0300

    cursor dos cards

--- src/components/dashboardCard/daschBoardCard.jsx
+++ src/components/dashboardCard/daschBoardCard.jsx
@@ -3,7 +3,7 @@ import {Icons} from "../../constants/icons.js"
 const DashBoardCard = ({titleCard,vendasTotal}) => {
     return (   <>
     
-        <div className="flex max-w-60 h-24 bg-blue-600 items-center justify-center tracking-wide font-semibold  rounded-md  text-white ">
+        <div className="flex max-w-60 h-24 bg-blue-600 hover:bg-blue-800  transition-colors cursor-pointer items-center justify-center tracking-wide font-semibold  rounded-md  text-white ">
 
             <div className="  flex flex-col items-center justify-center gap-6 p-3 text-center ">
                 <h4 className=" font-semibold">
