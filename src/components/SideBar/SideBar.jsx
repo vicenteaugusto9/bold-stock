5e841382d156ef5cf594a8b497c13fe6eba5f9d6
vicenteaugusto9 <vicenteaugusto180374@gmail.com>
2025-05-22 19:03:19 -0300

    Arquivo do sid bar inicilizado e instalacao das dependecias lucide-react e recharts

---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,11 @@
+import { Box,Settings,ShoppingCart,Archive,BarChart,LayoutDashboard} from "lucide-react";
+
+const SideBar = () => {
+    return ( <>
+    
+        
+    
+    </> );
+}
+ 
+export default SideBar;
