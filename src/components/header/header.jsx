2bb52582b85f47bd8323d1f7f1437f49448f7d5c
vicenteaugusto9 <vicenteaugusto180374@gmail.com>
2025-05-24 00:44:23 -0300

    novas dependcias e criacao de jsconfig

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,33 +1,20 @@
 import { Icons } from "../../constants/icons";
+import { User,ArrowDown,ChevronDown} from "lucide-react";
+import { DropdownMenu } from "@radix-ui/react-dropdown-menu";
 
 
-const Header = ({logo,iconInstagram,iconLinkedin,iconGithub}) => {
+const Header = ({users}) => {
     return ( 
 
         <>
-            <div className="flex border-b-blue-500 rounded-sm p-3 bg-gray-50  items-center  justify-evenly flex-wrap " >
-
-                
-                <div className="flex align-center items-center gap-1.5">
-
-                <img src={logo} alt="Logo Bold Stock" className="w-12 h-12  rounded-sm  "/>
-                    <p  className=" flex gap-1.5 text-sm  tracking-tighter text-orange-400 font-bold"> <p className="text-blue-900 text-sm tracking-tight font-bold" > Bold </p> Stock</p>
-                </div>
-                <div className="title">
-                    <h1 className=" tracking-wider text-4xl font-bold text-blue-900 ">
-                        DashBoard
-                    </h1>
-                </div>
-
-                <div className="flex gap-1.5">
-                    <img src={iconGithub} alt="Github" className="w-5 h-5 hover:scale-110 transition-transform duration-200 "  />
-                    <img src={iconInstagram} alt="instagram"  className="w-5 h-5 hover:scale-110 transition-transform duration-200"/>
-                    <img src={iconLinkedin} alt="Linkedin" className="w-5 h-5 hover:scale-110 transition-transform duration-200z    " />
-                </div>
-
-
-
+        <div className="flex  justify-end items-center pr-10 p-2">
+            <div className="flex items-center justify-end border gap-2 p-2 rounded-lg w-60 " >
+                <User className="w-5 h-5"/>
+                <p>{users}</p>
+                <ChevronDown className="w-5 h-5"/>
             </div>
+        </div>
+
         
         
         </>
--- src/utils/App.jsx
+++ src/utils/App.jsx
@@ -1,47 +1,28 @@
-import { useState } from 'react'
-import Header from '../components/header/header'
-import './global.css'
-import { Icons } from '../constants/icons'
-import DashBoardCard from '../components/dashboardCard/daschBoardCard'
-import GraficoDashBoard from '../components/GraficoDashBoard/Grafico'
-import SideBar from '../components/SideBar/SideBar'
+import { useState } from "react";
+import Header from "../components/header/header";
+import "./global.css";
+import { Icons } from "../constants/icons";
+import DashBoardCard from "../components/dashboardCard/daschBoardCard";
+import GraficoDashBoard from "../components/GraficoDashBoard/Grafico";
+import SideBar from "../components/SideBar/SideBar";
 
 function App() {
-  
-
   return (
- <>
- <div className="w-full  flex ">
-   <SideBar/>
-    <div className="flex flex-col">
-
-  <Header
-  logo={Icons.Logo}
-  iconLinkedin={Icons.Linkedin}
-  iconGithub={Icons.Github}
-  iconInstagram={Icons.Instagram}
-  />
-  <div className="flex gap-12 flex-wrap justify-center items-center pt-3">
-
-  <DashBoardCard
-  titleCard="Vendas Do Dia "
-  vendasTotal= "1.250,00"
-  
-  />
-  <DashBoardCard
-  titleCard="Vendas Do Mes "
-  vendasTotal= "35.100,00"
-  
-  />
-  </div>
-  <div className="p-4">
-    <GraficoDashBoard/>
-
-  </div>
-  
-  </div>
-  </div>
- </>
-  )
+    <>
+      <div className=" flex ">
+        <SideBar />
+        <div className="flex flex-col w-full ">
+          <Header users="Administrador" />
+          <div className="flex gap-12 flex-wrap justify-center items-center pt-3">
+            <DashBoardCard titleCard="Vendas Do Dia " vendasTotal="1.250,00" />
+            <DashBoardCard titleCard="Vendas Do Mes " vendasTotal="35.100,00" />
+          </div>
+          <div className="p-4">
+            <GraficoDashBoard />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
-export default App
+export default App;
