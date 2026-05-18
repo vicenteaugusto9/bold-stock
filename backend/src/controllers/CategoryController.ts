9cd9c5184ff40c4dbc36b2fb08376e661d40237b
vicenteaugusto9 <vicenteaugusto180374@gmail.com>
2026-05-18 11:51:00 -0300

    Refatorar o CategoryController para aprimorar o tratamento de erros e as mensagens de resposta; implementar validação para os métodos create, update e delete.

--- backend/src/controllers/CategoryController.ts
+++ backend/src/controllers/CategoryController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { CategoryService } from "../services/CategorySevice";
+import { error } from "node:console";
 
 export class CategoryController {
     private categoryService: CategoryService;
@@ -25,5 +26,61 @@ export class CategoryController {
             return res.status(500).json({ error: "Erro interno do servidor" });
         }
         
+    }
+    async listAll(req: Request , res: Response) {
+        try{
+            const result = await this.categoryService.listAllCategories();
+            return res.status(201).json(result)
+
+        } catch (error: any){
+            if (error.message === "Nenhuma categoria encontrada")
+                return res.status(404).json({error:error.message })
+        }
+        return res.status(500).json({ error: "Erro interno no servidor "}) 
+        
+    }
+
+    async update( req:Request , res: Response){
+        try {
+            const id = req.params.id as string; 
+            const {name} = req.body
+
+            if (!name) {
+                return res.status(400).json({error: "Nome da categoria e obrigatorio"})
+            }
+            const result = await this.categoryService.updateCategory(id , name)
+            return res.status(200).json(result)
+    
+        } catch (error:any){
+            if (error.message === "Categoria nao encontrada") {
+                return res.status(404).json({error:error.message})
+            }
+
+            return res.status(500).json({error: " Erro interno do servidor "})
+            
+        }
+    }
+
+    async delete( req:Request, res:Response){
+        try {
+            const id = req.params.id as string 
+
+            if (!id){
+                return res.status(400).json({error:" ID e obrigatorio"})
+          }
+            const result = await this.categoryService.deleteCategory(id)
+            return res.status(200).json(result)
+        } catch (error: any) {
+            if (error.message === "categoria nao encontrada"){
+                return res.status(404).json({error: error.message})}
+            if (error.message.includes("Vinculados")){
+                return res.status(409).json({error:error.message})
+            }    
+            
+           return res.status(500).json({error: "Erro interno do servidor "})
+
+        }
+        
+
     }
 }
