182e7a872ae0ac02ff536d1e8b6394579adea0cc
vicenteaugusto9 <vicenteaugusto180374@gmail.com>
2026-05-26 22:51:31 -0300

    organizacao de pastas

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './frontend'),
+      '@': path.resolve(__dirname, './frontend/src'),
     },
   },
 })
