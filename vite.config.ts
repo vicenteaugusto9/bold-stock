75c909ad1687cd9dba983df67c054e6da299abb6
vicenteaugusto9 <vicenteaugusto180374@gmail.com>
2026-05-27 20:50:01 -0300

    Refatoracao de arquivos e troca de stack para ficar padronizado com o backend

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: 'frontend',           // ✅ aponta para a pasta frontend
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './frontend/src'),
-    },
-  },
+      '@': path.resolve(__dirname, './frontend/src')
+    }
+  }
 })
